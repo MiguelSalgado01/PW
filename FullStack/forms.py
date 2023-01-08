86e127871fcc5bf330c264c8bc43a722a034fc9c
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2023-01-08 13:26:13 +0000

    register page missing only confirmation from db

--- FullStack/forms.py
+++ FullStack/forms.py
@@ -9,4 +9,11 @@ class UserRegisterForm(FlaskForm):
     password = PasswordField(label="Password", validators=[InputRequired(),  Length(min=8, max=20)])
     confirm_password = PasswordField(label="Confirmar Password", validators=[InputRequired(), EqualTo("password")])
     user_gender =  SelectField(label="Genero", validators=[DataRequired()], choices=[(" ","Selecionar Genero"), ("0","Feminino"), ("1","Masculino")])
-    submit = SubmitField(label="Registar")
+    submitRegist = SubmitField(label="Registar")
+    toLogin = SubmitField(label="Login")
+
+class UserLoginForm(FlaskForm):
+    student_number = StringField(label="Numero Estudante", validators=[InputRequired(), Length(min=9, max=9)])
+    password = PasswordField(label="Password", validators=[InputRequired(),  Length(min=8, max=20)])
+    login = SubmitField(label="Login")
+    toRegist = SubmitField(label="Cria Conta")
