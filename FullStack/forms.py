740826dbfe815e26dfe6679a35d543e35bb1f428
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2023-01-11 14:11:05 +0000

    new user

--- FullStack/forms.py
+++ FullStack/forms.py
@@ -10,10 +10,19 @@ class UserRegisterForm(FlaskForm):
     confirm_password = PasswordField(label="Confirmar Password", validators=[InputRequired(), EqualTo("password")])
     user_gender =  SelectField(label="Genero", validators=[DataRequired()], choices=[(" ","Selecionar Genero"), ("0","Feminino"), ("1","Masculino")])
     submitRegist = SubmitField(label="Registar")
-    toLogin = SubmitField(label="Login")
 
 class UserLoginForm(FlaskForm):
     student_number = StringField(label="Numero Estudante", validators=[InputRequired(), Length(min=9, max=9)])
     password = PasswordField(label="Password", validators=[InputRequired(),  Length(min=8, max=20)])
     login = SubmitField(label="Login")
     toRegist = SubmitField(label="Cria Conta")
+
+    
+class RideForm(FlaskForm):
+    vehicle = StringField(label="veiculo", validators=[InputRequired(), Length(min=9, max=9)])
+    data = StringField(label="data", validators=[InputRequired(),  Length(min=8, max=20)])
+    hora = StringField(label="hora", validators=[InputRequired(),  Length(min=8, max=20)])
+    place = StringField(label="local", validators=[InputRequired(),  Length(min=8, max=20)])
+    comGo = StringField(label="comeGo", validators=[InputRequired(),  Length(min=8, max=20)])
+    comGo = StringField(label="comeGo", validators=[Length(min=8, max=20)])
+    toRegist = SubmitField(label="Add Veiculo")
---
+++ FullStack/rides/__init__.py
@@ -0,0 +1,50 @@
+from flask import Blueprint, request
+from flask import redirect, render_template, url_for
+from forms import RideForm
+from models import User, db
+
+rides = Blueprint('rides', __name__)
+
+@rides.route('/rides', methods=['GET', 'POST'])
+def doLogin():
+    return render_template("criarBoleia.html")
+
+
+
+from flask import Blueprint
+from flask import redirect, render_template, url_for
+from forms import UserRegisterForm
+
+modulo2 = Blueprint('modulo2', __name__)
+
+@modulo2.route('/registar', methods=['GET', 'POST'])
+def register():
+    form = UserRegisterForm()
+
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            verifyStudentNumb = db.session.query(User).filter(User.student_number == form.student_number.data).first()
+            verifyUsername = db.session.query(User).filter(User.name == form.username.data).first()
+            if(verifyStudentNumb == None and verifyUsername == None):
+                try:
+                    new_user = User(name=form.username.data,student_number=form.student_number.data,phone_number=form.phone_number.data,password=form.password.data,gender=form.user_gender.data)
+                    db.session.add(new_user)
+                    # db.session.commit()
+                    return redirect('/login')
+                except:
+                    return 'error'
+                    
+            elif(verifyStudentNumb != None and verifyUsername != None):
+                form.student_number.errors.append("Student number already exists")
+                form.username.errors.append("Username number already exists")
+            elif(verifyStudentNumb != None):
+                form.student_number.errors.append("Student number already exists")
+            elif(verifyUsername != None):
+                form.username.errors.append("Username number already exists")
+        
+        elif request.form['LogBtn'] == 'Tenho Conta':
+            return redirect('/login')
+       
+    elif request.method == 'GET': 
+        return render_template("registarUser.html", title="Registar User", form=form)
+  
