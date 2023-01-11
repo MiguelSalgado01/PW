cdb84501898303c0489aececdbdbecdab447245a
MiguelSalgado01 <83299538+MiguelSalgado01@users.noreply.github.com>
2023-01-11 23:06:11 +0000

    Show database value in html page working

--- FullStack/modulo3/__init__.py
+++ FullStack/modulo3/__init__.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import redirect, render_template, url_for
-from models import Reservation
+from models import Reservation,Ride,db,User
 
 modulo3 = Blueprint('modulo3', __name__)
 
@@ -8,3 +8,14 @@ modulo3 = Blueprint('modulo3', __name__)
 @modulo3.route('/reserva', methods=['GET', 'POST'])
 def reservation():
     return render_template("reservas.html")
+
+@modulo3.route('/boleia', methods=['GET', 'POST'])
+def ride():
+    #return render_template("pesquisa.html")
+    #getUserData = db.session.query(User).all()
+    getUserData = db.session.query(User).all()
+    return render_template("pesquisa.html",getUserData=getUserData)
+
+        
+      
+
