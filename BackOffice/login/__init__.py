84be8a7015d7c1ab9338e8b000f5ef21e4fa67a4
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2023-01-15 22:47:43 +0000

    massive change in BackOffice

--- BackOffice/login/__init__.py
+++ BackOffice/login/__init__.py
@@ -26,27 +26,23 @@ def doLogin():
     if request.method == 'POST':
         if form.validate_on_submit() and form.login.data == True:
             user = db.session.query(User).filter(User.student_number == form.student_number.data).first()
-            if(user != None):
-                 admin_id = db.session.query(Admin).filter(Admin.user_id == user.id).first()
-            
-                 if ( admin_id != None):
-                     
-                         if (User.student_number == 0):
-                                 form.student_number.errors.append("Incorrect Student Number")
-                        
-                         else:
-                                if(bcrypt.check_password_hash(user.password, form.password.data)):
-                                    #try:
-                                    user.active = True
-                                    user.last_login_date = datetime.now()
-                                    db.session.commit()
-                                login_user(user)
-                                return redirect('usersPage')
-                            #except:
-                                # return 'error'
+            admin_id = db.session.query(Admin).filter(Admin.user_id == user.id).first()
+           
+            if (admin_id == None):
+                 form.student_number.errors.append("Incorrect Student Number")
+            else:
+                 if(bcrypt.check_password_hash(user.password, form.password.data)):
+                    #try:
+                        user.active = True
+                        user.last_login_date = datetime.now()
+                        db.session.commit()
+                        login_user(user)
+                        return redirect('usersPage')
+                    #except:
+                        # return 'error'
                  else:
-                                form.password.errors.append("Incorrect Password")
-            
+                    form.password.errors.append("Incorrect Password")
+    
     return render_template("pages-sign-in.html", title="Login", formFront=form)
 
 
