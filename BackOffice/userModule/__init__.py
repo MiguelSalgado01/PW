9b33fc07252c484671aa9ab98378f86e84c508e9
MiguelSalgado01 <83299538+MiguelSalgado01@users.noreply.github.com>
2023-01-16 11:46:19 +0000

    update user delete

--- BackOffice/userModule/__init__.py
+++ BackOffice/userModule/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,request
 from flask import redirect, render_template, url_for, jsonify
 from forms import AdminForm
-from models import db , Admin,User
+from models import db , Admin,User,Ride,Vehicle,Reservation
 from datetime import datetime
 from flask_login import current_user
 
@@ -21,8 +21,14 @@ def toUsersPage():
         id = request.form.get("id")
         print(id)
         specify_User = db.session.query(User).filter(User.id==id).first()
-        print(specify_User)
+        specify_Ride = db.session.query(Ride).filter(Ride.user_id==specify_User.id).first()
+        specify_Vehicle = db.session.query(Vehicle).filter(Vehicle.owner_id==specify_User.id).first()
+        specify_Reservation = db.session.query(Reservation).filter(Reservation.passenger_id==specify_User.id).first()
+
         db.session.delete(specify_User)
+        db.session.delete(specify_Ride)
+        db.session.delete(specify_Vehicle)
+        db.session.delete(specify_Reservation)
         db.session.commit()
         
        
