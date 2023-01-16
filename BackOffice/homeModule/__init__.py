4c4b7dff76ad92708c40c24984013249eba3c300
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2023-01-16 00:39:40 +0000

    dashbord almost done

--- BackOffice/homeModule/__init__.py
+++ BackOffice/homeModule/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,request
 from flask import redirect, render_template, url_for
 from forms import AdminForm
-from models import db , Admin,User, Ride, Reservation
+from models import db , Admin,User, Ride, Reservation, Vehicle
 from datetime import datetime
 from flask_login import current_user
 
@@ -15,11 +15,37 @@ def toHomePage():
     else:
         print("Not Noice")
 
-    get_Users = db.session.query(User).all()
+    use_id = current_user.id
+
+    activeUser =  db.session.query(User).filter(User.id==use_id).first()
+
+    allUsers = db.session.query(User).all()
     allRides = db.session.query(Ride).all()
     allReservations = db.session.query(Reservation).all()
+    allVehicles = db.session.query(Vehicle).all()
+
+    allUsersCount = len(allUsers)
+    allRidesCount = len(allRides)
+    allReservationsCount = len(allReservations)
+    allVehiclesCount = len(allVehicles)
+
+    ridesFormated = []
+
+    # for us in allUsers:
+    #     print(us)
+
+    for rs in allRides:
+        print(rs)
+        datetime_object = datetime.strptime(rs.ride_date, "%Y-%M-%d").date()
+        rs.ride_date = datetime_object
+
+    # for res in allReservations:
+    #     print(res)
+
     form = AdminForm()
-    return render_template("home.html", title="Login", get_Users = get_Users, allRides = allRides, allReservations = allReservations)
+    return render_template("home.html", title="Login", allUsers = allUsers, allRides = allRides, 
+        allReservations = allReservations, allRidesCount = allRidesCount, allReservationsCount = allReservationsCount, 
+        allVehiclesCount = allVehiclesCount, allUsersCount = allUsersCount, activeUser = activeUser)
 
 
 
