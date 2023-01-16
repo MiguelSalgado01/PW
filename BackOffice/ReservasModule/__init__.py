eb94fd346a79d15bfeecdc232e345f6006d88ed3
TiagoCaetanoReal <tiagomiguelbaraocaetano@gmail.com>
2023-01-16 02:19:53 +0000

    home looking good

--- BackOffice/ReservasModule/__init__.py
+++ BackOffice/ReservasModule/__init__.py
@@ -1,7 +1,7 @@
 from flask import Blueprint,request
 from flask import redirect, render_template, url_for
 from forms import AdminForm
-from models import db , Reservation, User, bcrypt
+from models import db , Reservation, User, bcrypt, ReservationState
 from flask_login import login_user, logout_user, current_user
 from datetime import datetime
 
@@ -17,6 +17,6 @@ def toReservaPage():
         print("Noice")
     else:
         print("Not Noice")
-    get_Reserva= db.session.query(Reservation).all()
+    get_Reserva= db.session.query(Reservation, ReservationState).filter(Reservation.reservation_state_id == ReservationState.id).all()
     form = AdminForm()
     return render_template("reservas.html", title="Login", get_Reserva = get_Reserva)
--- BackOffice/homeModule/__init__.py
+++ BackOffice/homeModule/__init__.py
@@ -1,15 +1,20 @@
 from flask import Blueprint,request
 from flask import redirect, render_template, url_for
 from forms import AdminForm
-from models import db , Admin,User, Ride, Reservation, Vehicle
+from models import db , Admin, User, Ride, Reservation, Vehicle
+from sqlalchemy import func, desc
 from datetime import datetime
 from flask_login import current_user
 
 homeModule = Blueprint('homeModule', __name__)
 
 
+
 @homeModule.route('/homePage', methods=['GET', 'POST'])
 def toHomePage():
+    dates = []
+    graf_topVehicles = []
+
     if current_user.is_authenticated:
         print("Noice")
     else:
@@ -29,23 +34,33 @@ def toHomePage():
     allReservationsCount = len(allReservations)
     allVehiclesCount = len(allVehicles)
 
-    ridesFormated = []
-
-    # for us in allUsers:
-    #     print(us)
-
     for rs in allRides:
-        print(rs)
-        datetime_object = datetime.strptime(rs.ride_date, "%Y-%M-%d").date()
+        datetime_object = datetime.strptime(rs.ride_date, "%Y-%m-%d").date()
         rs.ride_date = datetime_object
+        dates.append(rs.ride_date.month)
+    
+    graf_dataBoleias = [("Jan", dates.count(1)), ("Fev", dates.count(2)),("Mar", dates.count(3)), ("Abr", dates.count(4)),
+        ("Mai", dates.count(5)), ("Jun", dates.count(6)),("Jul", dates.count(7)), ("Ago", dates.count(8)),
+        ("Set", dates.count(9)), ("Out", dates.count(10)),("Nov", dates.count(11)), ("Dez", dates.count(12)),]
 
-    # for res in allReservations:
-    #     print(res)
+
+    topVehicles = db.session.query(Ride,func.count(Ride.vehicle_id).label('qty')).group_by(Ride.vehicle_id).order_by(desc('qty')).limit(3).all()
+
+    for v in topVehicles:
+        print(v.Ride.vehicle_id)
+        vehiclePerRide = db.session.query(Vehicle).filter(v.Ride.vehicle_id == Vehicle.id).first()
+        print(vehiclePerRide.license_plate)
+        graf_topVehicles.append((vehiclePerRide.license_plate, v.qty))
+
+    print(graf_topVehicles)
+    print(topVehicles[0].qty)
+    print(topVehicles[1].qty)
+    print(topVehicles[2].qty)
 
     form = AdminForm()
-    return render_template("home.html", title="Login", allUsers = allUsers, allRides = allRides, 
-        allReservations = allReservations, allRidesCount = allRidesCount, allReservationsCount = allReservationsCount, 
-        allVehiclesCount = allVehiclesCount, allUsersCount = allUsersCount, activeUser = activeUser)
+    return render_template("home.html", title="Login", allRidesCount = allRidesCount, allReservationsCount = allReservationsCount, 
+        allVehiclesCount = allVehiclesCount, allUsersCount = allUsersCount, activeUser = activeUser, 
+        graf_dataBoleias = graf_dataBoleias, graf_topVehicles = graf_topVehicles)
 
 
 
